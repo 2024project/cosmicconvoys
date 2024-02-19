717a937e83ad2f639424b32e4c183087e03bdf13
obscuredc <austinberm@gmail.com>
2024-02-18 19:55:54 -0500

    Initial5

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Import packages as required.
 
 // The dotenv package enables us to read the values from .env:
-// the config() function exposes the information contained.
+// the config() function exposes the information contained. Ex: process.env.PORT .
 require('dotenv').config();
+
+console.log(process.env.PORT);
