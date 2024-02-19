92b2d2f4cddccde1e3352112d310da875b012461
obscuredc <austinberm@gmail.com>
2024-02-18 19:53:42 -0500

    Initial4

---
+++ index.js
@@ -0,0 +1,5 @@
+// Import packages as required.
+
+// The dotenv package enables us to read the values from .env:
+// the config() function exposes the information contained.
+require('dotenv').config();
